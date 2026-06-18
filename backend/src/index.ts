eb6e8b427287c7b0a839f706047c193927b9fede
EmilySouza22 <emilyseafonso@gmail.com>
2026-06-18 17:06:58 -0300

    feat: registrando novas rotas no aplicação

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { auth } from './middleware/auth';
 import cors from "cors"
+
+import { auth } from './middleware/auth';
+
 import { authRouter } from './routes/auth';
 import { usuarioRouter } from './routes/usuario';
 import { exameRouter } from './routes/exame';
+import { consultaRouter } from "./routes/consulta";
+import { pacienteRouter } from "./routes/paciente";
 
 const app = express();
 app.use(express.json())
@@ -17,11 +21,12 @@ app.get('/', (req, res) => {
 
 app.use(authRouter)
 
-//midleware de autenticação
 app.use(auth)
 
 app.use(usuarioRouter)
 app.use(exameRouter)
+app.use(consultaRouter)
+app.use(pacienteRouter)
 
 app.listen(port, () => {
   console.log("Servidor ta de pé :p")
