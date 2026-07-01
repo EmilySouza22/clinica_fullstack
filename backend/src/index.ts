f4857ac49316e12be48f7a46e70205214917c858
EmilySouza22 <emilyseafonso@gmail.com>
2026-07-01 17:13:10 -0300

    feat(routes): adiciona rotas do prontuario no index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,36 @@
 import express from 'express';
-import cors from "cors"
+import cors from 'cors';
 
 import { auth } from './middleware/auth';
 
 import { authRouter } from './routes/auth';
 import { usuarioRouter, usuarioPublicRouter } from './routes/usuario';
 import { exameRouter } from './routes/exame';
-import { consultaRouter } from "./routes/consulta";
-import { pacienteRouter } from "./routes/paciente";
+import { consultaRouter } from './routes/consulta';
+import { pacienteRouter } from './routes/paciente';
+import { prontuarioRouter } from './routes/prontuario';
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => {
-  console.log(req)
-  res.send("Hello world")
-})
+	console.log(req);
+	res.send('Hello world');
+});
 
-app.use(authRouter)
-app.use(usuarioPublicRouter) // cadastro, sem auth
+app.use(authRouter);
+app.use(usuarioPublicRouter); // cadastro, sem auth
 
-app.use(auth) // a partir daqui, tudo exige token
+app.use(auth); // a partir daqui, tudo exige token
 
-app.use(usuarioRouter)
-app.use(exameRouter)
-app.use(consultaRouter)
-app.use(pacienteRouter)
+app.use(usuarioRouter);
+app.use(exameRouter);
+app.use(consultaRouter);
+app.use(pacienteRouter);
+app.use(prontuarioRouter);
 
 app.listen(port, () => {
-  console.log("Servidor ta de pé :p")
-})
+	console.log('Servidor ta de pé :p');
+});
