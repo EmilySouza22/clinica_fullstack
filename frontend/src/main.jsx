a23cc1df0f4c8465b6b691f0a4a72b7763fbbc84
EmilySouza22 <emilyseafonso@gmail.com>
2026-07-06 21:07:10 -0300

    feat(routes): ajustando main e separando rotas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,52 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
-//react router
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-
-//toastify
+import { RouterProvider } from 'react-router/dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
 import { AuthProvider } from './contexts/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import DashboardLayout from './layouts/DashboardLayout';
-import MedicalRecordList from './components/MedicalRecordList';
-import RegisterFormPatient from './components/RegisterFormPatient';
-import ConsultationForm from './components/ConsultationForm';
-import RegisterFormExam from './components/RegisterFormExam/index'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />
-  },
-  {
-
-    element: (
-      <PrivateRoute>
-        <DashboardLayout />
-      </PrivateRoute>
-    ),
-    children: [
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/prontuarios", element: <MedicalRecordList /> },
-      { path: "/pacientes", element: <RegisterFormPatient /> },
-      { path: "/consultas", element: <ConsultationForm /> },
-      { path: "/exames", element: <RegisterFormExam /> },
-    ]
-  }
-]);
+import router from './routes/AppRoutes';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <ToastContainer />
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>,
-)
+	<StrictMode>
+		<AuthProvider>
+			<ToastContainer />
+			<RouterProvider router={router} />
+		</AuthProvider>
+	</StrictMode>,
+);
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import PrivateRoute from '../components/PrivateRoute';
+import DashboardLayout from '../layouts/DashboardLayout';
+import MedicalRecordList from '../components/MedicalRecordList';
+import RegisterFormPatient from '../components/RegisterFormPatient';
+import ConsultationForm from '../components/ConsultationForm';
+import RegisterFormExam from '../components/RegisterFormExam';
+
+const router = createBrowserRouter([
+	{ path: '/', element: <Login /> },
+	{
+		element: (
+			<PrivateRoute>
+				<DashboardLayout />
+			</PrivateRoute>
+		),
+		children: [
+			{ path: '/dashboard', element: <Dashboard /> },
+			{ path: '/prontuarios', element: <MedicalRecordList /> },
+			{ path: '/pacientes', element: <RegisterFormPatient /> },
+			{ path: '/consultas', element: <ConsultationForm /> },
+			{ path: '/exames', element: <RegisterFormExam /> },
+		],
+	},
+]);
+
+export default router;
